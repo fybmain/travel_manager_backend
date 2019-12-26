4cd26d2da7d49df7e9a9afb44d048f31324f56de
HumbertZhang <504490160@qq.com>
2019-12-26 11:25:39 +0800

    init

---
+++ src/main/java/com/example/travelmanager/TravelmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmanagerApplication.class, args);
+    }
+
+}
