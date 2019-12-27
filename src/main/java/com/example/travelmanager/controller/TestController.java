c66c24c5c5ef7d6414391ab3a53c1af709626765
Chenglei Yuan <Chenglei.Y@outlook.com>
2019-12-27 16:52:38 +0800

    add mysql connection

--- src/main/java/com/example/travelmanager/controller/TestController.java
+++ src/main/java/com/example/travelmanager/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.example.travelmanager.controller;
 
+import com.example.travelmanager.dao.UserRepository;
+import com.example.travelmanager.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api")
@@ -10,6 +12,20 @@ public class TestController {
     @RequestMapping("/test")
     public String test() {
         return "Test";
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/adduser")
+    public User addUser(@RequestParam String userName, @RequestParam String name)
+    {
+        User u = new User();
+        u.setUserName(userName);
+        u.setName(name);
+        userRepository.save(u);
+
+        return u;
     }
 }
 
