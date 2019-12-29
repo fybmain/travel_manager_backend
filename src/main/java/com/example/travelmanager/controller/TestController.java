e4a56494901317a2c6b217366fa42cb6f5581047
Chenglei Yuan <Chenglei.Y@outlook.com>
2019-12-29 11:06:17 +0800

    修改数据库接口类

--- src/main/java/com/example/travelmanager/controller/TestController.java
+++ src/main/java/com/example/travelmanager/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.example.travelmanager.controller;
 
-import com.example.travelmanager.dao.UserRepository;
+import com.example.travelmanager.dao.UserDao;
 import com.example.travelmanager.service.auth.AuthException.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +18,7 @@ public class TestController {
     }
 
     @Autowired
-    private UserRepository userRepository;
+    private UserDao userDao;
 
     @RequestMapping("/token")
     public String addUser(@RequestParam String userName, @RequestParam String name)
