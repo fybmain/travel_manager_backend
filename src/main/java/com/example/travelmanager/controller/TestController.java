7e268df31ab170b61ed722859271bb9472e3d8ce
HumbertZhang <504490160@qq.com>
2019-12-30 16:19:31 +0800

    fix: ResultBean, its retrun ResponseEntity now

--- src/main/java/com/example/travelmanager/controller/TestController.java
+++ src/main/java/com/example/travelmanager/controller/TestController.java
@@ -5,6 +5,8 @@ import com.example.travelmanager.dao.UserDao;
 import com.example.travelmanager.entity.User;
 import com.example.travelmanager.service.auth.AuthException.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +26,11 @@ public class TestController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public ResultBean testGetUser() {
+    public ResponseEntity testGetUser() {
         User u = new User();
         u.setName("test");
-        return ResultBean.success(u);
+
+        return ResultBean.success(HttpStatus.CREATED, u);
     }
 
 }
