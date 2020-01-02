cf1fec589a92c1963515f6e7b9821dc9b3fc8ac6
Chenglei Yuan <Chenglei.Y@outlook.com>
2020-01-02 17:18:05 +0800

    use simplify travelapplication response

--- src/main/java/com/example/travelmanager/controller/TestController.java
+++ src/main/java/com/example/travelmanager/controller/TestController.java
@@ -2,15 +2,21 @@ package com.example.travelmanager.controller;
 
 import com.example.travelmanager.config.Constant;
 import com.example.travelmanager.controller.bean.ResultBean;
+import com.example.travelmanager.dao.TravelApplicationDao;
 import com.example.travelmanager.dao.UserDao;
-import com.example.travelmanager.enums.UserRoleEnum;
 import com.example.travelmanager.service.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
@@ -21,10 +27,17 @@ public class TestController {
     @Autowired
     private AuthService authService;
 
-    @RequestMapping("/access")
+    @Autowired
+    private TravelApplicationDao travelApplicationDao;
+
+    @RequestMapping("/test")
     @ResponseBody
     public HttpEntity access(@RequestHeader(Constant.HEADER_STRING) String auth) {
-        int uid = authService.authorize(auth, UserRoleEnum.Employee, UserRoleEnum.DepartmentManager);
-        return ResultBean.success();
+        int uid = authService.authorize(auth);
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(1);
+        set.add(2);
+        Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "id");
+        return ResultBean.success(travelApplicationDao.findAllWithState(set, pageable).toList());
     }
 }
--- src/main/java/com/example/travelmanager/dao/TravelApplicationDao.java
+++ src/main/java/com/example/travelmanager/dao/TravelApplicationDao.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
+
 public interface TravelApplicationDao extends CrudRepository<TravelApplication, Integer> , JpaSpecificationExecutor<TravelApplication> {
     @Query("select t from TravelApplication t where t.applicantId = :uid")
     Page<TravelApplication> finaAllByApplicantId(@Param("uid") int uid, Pageable pageable);
@@ -17,4 +19,7 @@ public interface TravelApplicationDao extends CrudRepository<TravelApplication, 
 
     @Query("select t from TravelApplication t where t.applicantId = :uid and (t.status = 1 or t.status = 2)")
     Page<TravelApplication> finaAllByApplicantIdUnFinished(@Param("uid") int uid, Pageable pageable);
+
+    @Query("select t from TravelApplication t where t.status in :statusSet")
+    Page<TravelApplication> findAllWithState(@Param("statusSet")Set<Integer> set, Pageable pageable);
 }
