861935cd71fbebae028e63e0228c8b872a6fd0a2
Chenglei Yuan <Chenglei.Y@outlook.com>
2020-01-03 15:55:10 +0800

    add reset password

--- src/main/java/com/example/travelmanager/config/WebException/AuthControllerException.java
+++ src/main/java/com/example/travelmanager/config/WebException/AuthControllerException.java
@@ -6,9 +6,15 @@ public class AuthControllerException extends ErrorException {
     private static final Integer WorkIdExists = 1001;
     private static final String WorkIdExistsMsg = "work id exists";
     public static final AuthControllerException WorkIdNotExistException =
-            new AuthControllerException(WorkIdExists,WorkIdExistsMsg, HttpStatus.BAD_REQUEST);
+        new AuthControllerException(WorkIdExists,WorkIdExistsMsg, HttpStatus.BAD_REQUEST);
 
 
+    private static final Integer OldPasswordError = 1002;
+    private static final String OldPasswordErrorMsg = "密码错误";
+    public static final AuthControllerException OldPasswordErrorException = 
+        new AuthControllerException(OldPasswordError, OldPasswordErrorMsg, HttpStatus.BAD_REQUEST);
+
+    
     // 构造函数
     private AuthControllerException(int code, String msg, HttpStatus httpStatus) {
         super(code, msg, httpStatus);
