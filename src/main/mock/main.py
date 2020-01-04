20495116bceaf2b1d643835896d574f355cb4cd0
HumbertZhang <504490160@qq.com>
2020-01-04 17:20:00 +0800

    add mock

--- src/main/mock/main.py
+++ src/main/mock/main.py
@@ -1,5 +1,6 @@
 from db import Database
 from mocker import Mocker
+import random
 
 """
 1. insert admin
@@ -17,6 +18,9 @@ from mocker import Mocker
 
 
 class UserRole:
+    def __init__(self):
+        pass
+
     Employee = 0
     DepartmentManager = 1
     Manager = 2
@@ -24,6 +28,9 @@ class UserRole:
 
 
 class ApplicationStatus:
+    def __init__(self):
+        pass
+
     NeedDepartmentManagerApprove = 1
     NeedManagerApprove = 2
     ApplicationApproved = 3
@@ -41,12 +48,54 @@ def main():
     # 总经理 & 总经理办公室
     manager_id = mocker.insert_user(1, UserRole.Manager)
     department_id = mocker.insert_department(manager_id, "总办")
-    mocker.update_user_departmentId(department_id, manager_id)
+    mocker.update_user_department_id(department_id, manager_id)
 
     # 5个部门
     departs = ["云计算事业部", "物联网事业部", "互动娱乐事业部", "网络媒体事业部", "广告事业部"]
     for d in departs:
-        print(d)
+        depart_manager_id = mocker.insert_user(0, UserRole.DepartmentManager)
+        department_id = mocker.insert_department(depart_manager_id, d)
+        mocker.update_user_department_id(department_id, manager_id)
+
+        user_cnt = random.randint(30, 50)
+        for i in range(user_cnt):
+            uid = mocker.insert_user(department_id, UserRole.Employee)
+
+            # 3-5 travelApplication whith status canbe 1 2 or 4
+            travel_other_cnt = random.randint(3, 5)
+            for j in range(travel_other_cnt):
+                status_list = [ApplicationStatus.NeedManagerApprove, ApplicationStatus.NeedDepartmentManagerApprove, ApplicationStatus.ApplicationNotApproved]
+                status = status_list[random.randint(0, len(status_list)-1)]
+                mocker.insert_travel(uid, department_id, status, False)
+
+            # 3-10 travelApplication, （status is approved）, and 70% paid is true.
+            travel_ok_cnt = random.randint(3, 10)
+            # status is approved and paid is true
+            can_pay_travel_id_list = []
+            for k in range(travel_ok_cnt):
+                paid_rand = random.randint(30, 90)
+                paid = True
+                if paid_rand >= 60:
+                    paid = False
+                tid = mocker.insert_travel(uid, department_id, ApplicationStatus.ApplicationApproved, paid)
+                if paid:
+                    can_pay_travel_id_list.append(tid)
+
+            # 为这些travel创建payment
+            for tid in can_pay_travel_id_list:
+                pics = "https://picturesbed.oss-cn-hangzhou.aliyuncs.com/travelmanager/0520201254220450945697959433.png"
+                status_rand = random.randint(1, 100)
+                status = ApplicationStatus.ApplicationApproved
+                # status 70% is approved. 10% is unapproved  10%is 总经理审批  10% is 部门经理审批
+                if 70 < status_rand <= 80:
+                    status = ApplicationStatus.ApplicationNotApproved
+                elif 80 < status_rand <= 90:
+                    status = ApplicationStatus.NeedManagerApprove
+                elif 90 < status_rand <= 100:
+                    status = ApplicationStatus.NeedDepartmentManagerApprove
+
+                mocker.insert_payment(uid, department_id, pics, status, tid)
+
 
 if __name__ == '__main__':
     main()
--- src/main/mock/mocker.py
+++ src/main/mock/mocker.py
@@ -1,11 +1,15 @@
 """
 mocker
 """
+
 import random
 import string
 from datetime import datetime
 
-def randomString(stringLength=10):
+from name_generator import full_name, last_names, first_names
+
+
+def random_string(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
@@ -26,8 +30,10 @@ class Mocker:
         nid = self.get_next_id("user")+1
         fake_workid = str(random.randint(100000000, 999999999))
         fake_email = fake_workid + "@" + "qq.com"
-        fake_name = "MOCK" + randomString(8)
-        fake_hash = randomString(50)
+
+        fake_name = "MOCK" + full_name(last_names, first_names)
+
+        fake_hash = random_string(50)
         sql = ("INSERT INTO user (id, department_id, email, name, password_hash, role, status, telephone, work_id) " \
                "VALUES ({}, {}, '{}', '{}', '{}', {}, {}, '{}', '{}')".format(nid, department_id, fake_email, fake_name, fake_hash, role, 1, fake_workid, fake_workid))
 
@@ -39,37 +45,39 @@ class Mocker:
 
         return nid
 
-
-    def update_user_departmentId(self, department_id, user_id):
+    def update_user_department_id(self, department_id, user_id):
         sql = ("UPDATE user SET department_id = {} WHERE id = {}".format(department_id, user_id))
+        print("SQL:", sql)
         cursor = self.conn.cursor()
         cursor.execute(sql)
         self.conn.commit()
-
 
     def insert_department(self, manager_id, department_name):
         nid = self.get_next_id("department")+1
         department_name = "MOCK" + department_name
 
         sql = "INSERT INTO department (id, manager_id, name) VALUES ({}, {}, '{}')".format(nid, manager_id, department_name)
+        print("SQL:", sql)
         cursor = self.conn.cursor()
         cursor.execute(sql)
         self.conn.commit()
 
         return nid
 
-
     def update_department_manager(self, manager_id, department_id):
         sql = ("UPDATE department SET manager_id = {} WHERE department_id = {}".format(manager_id, department_id))
+        print("SQL:", sql)
         cursor = self.conn.cursor()
         cursor.execute(sql)
         self.conn.commit()
 
+    # NO USE
     def insert_picture(self, url):
         nid = self.get_next_id("picture")+1
         time = "2016-01-01"
 
         sql = "INSERT INTO picture (id, upload_time, url) VALUES ({}, '{}', '{}')".format(nid, time, url)
+        print("SQL:", sql)
         cursor = self.conn.cursor()
         cursor.execute(sql)
         self.conn.commit()
@@ -78,7 +86,7 @@ class Mocker:
 
     def insert_payment(self, applicant_id, department_id, pics, status, travel_id):
         nid = self.get_next_id("payment_application")+1
-        month = random.randint(1, 13)
+        month = random.randint(1, 12)
         monthStr = ""
         if month < 10:
             monthStr = "0" + str(month)
@@ -87,15 +95,15 @@ class Mocker:
 
         time_str = datetime.today().strftime("2019-" + monthStr + "-%d %H:%M:%S")
 
-        food = random.randint(10, 101)
-        hotel = random.randint(10, 101)
-        vehicle = random.randint(10, 101)
-        other = random.randint(10, 101)
+        food = random.randint(10, 100)
+        hotel = random.randint(10, 100)
+        vehicle = random.randint(10, 100)
+        other = random.randint(10, 100)
 
         sql = "INSERT INTO payment_application (id, applicant_id, apply_time, department_id, food_payment, hotel_payment, invoiceurls, other_payment, status, travel_id, vehicle_payment) " \
               "VALUES ({}, {}, '{}', {}, {}, {}, '{}', {}, {}, {}, {})"\
               .format(nid, applicant_id, time_str, department_id, food, hotel, pics, other, status, travel_id, vehicle)
-        print(sql)
+        print("SQL:", sql)
 
         cursor = self.conn.cursor()
         cursor.execute(sql)
@@ -110,28 +118,27 @@ class Mocker:
             '湖南省':['长沙','衡阳','湘潭','邵阳','岳阳','株洲'],
             '江西省':['南昌','九江','上饶','景德镇']
         }
-        province = list(dict.keys())[(random.randint(0, len(dict.keys())))]
+        province = list(dict.keys())[(random.randint(0, len(dict.keys())-1))]
         city_list = dict.get(province)
-        city = city_list[(random.randint(0, len(city_list)))]
+        city = city_list[(random.randint(0, len(city_list)-1))]
 
+        reasons = ["技术现场支持客户",
+                   "合作洽谈",
+                   "售卖服务",
+                   "参加会议"]
 
-        reasons = ["1",
-                   "2",
-                   "3",
-                   "4"]
-
-        reason = reasons[random.randint(0, len(reasons))]
+        reason = reasons[random.randint(0, len(reasons)-1)]
+        reason = "MOCK" + reason
 
         nid = self.get_next_id("travel_application")+1
         time_str = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
-        food = random.randint(10, 101)
-        hotel = random.randint(10, 101)
-        vehicle = random.randint(10, 101)
-        other = random.randint(10, 101)
+        food = random.randint(10, 100)
+        hotel = random.randint(10, 100)
+        vehicle = random.randint(10, 100)
+        other = random.randint(10, 100)
 
-
-        month = random.randint(1, 13)
+        month = random.randint(1, 12)
         monthStr = ""
         if month < 10:
             monthStr = "0" + str(month)
@@ -149,7 +156,7 @@ class Mocker:
               "VALUES ({}, {}, '{}', '{}', {}, '{}', {}, {}, {}, {}, '{}', '{}', '{}', {}, {})"\
               .format(nid, applicant_id, time_now, city, department_id, end_time, food, hotel, other, paid, province, reason, start_time, status, vehicle)
 
-        print(sql)
+        print("SQL:", sql)
 
         cursor = self.conn.cursor()
         cursor.execute(sql)
